a75d09787209043a8aa2091fcd93803f76dc0995
fushijia <2812719509@qq.com>
2019-02-25 16:23:48 +0800

    weixin

--- resources/views/admin/service.blade.php
+++ resources/views/admin/service.blade.php
@@ -54,15 +54,15 @@
                 "massage",
                 {openid:openid},
                 function(msg){
-                    console.log(msg);
-                    /*for(var i in msg['data']){
+
+                    for(var i in msg['data']){
                         _tr+=
                             "<tr>" +
                             "<td>"+"<h3>"+msg['nickname']+":"+"</h3>"+"</td>" +
                             "<td>"+msg['data'][i]['message']+"</td>" +
                             "</tr>"
 
-                    }*/
+                    }
                     $('#show').html(_tr);
                 },'json'
             )
