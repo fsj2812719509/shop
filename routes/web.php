a34d93da6efd56fb08602c41477377ce7bbebace
fushijia <2812719509@qq.com>
2019-02-26 15:08:54 +0800

    weixin

--- routes/web.php
+++ routes/web.php
@@ -110,5 +110,12 @@ Route::post('/Wechat/creatmenu','Wechat\WechatController@createMenu');
 
 
 
+//微信支付
+Route::get('/weixin/pay/test','Weixin\PayController@test');     //微信支付测试
+Route::post('/weixin/pay/notice','Weixin\PayController@notice');     //微信支付通知回调
+
+
+
+
 
 
