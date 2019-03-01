7cb609cf6bcebcb9ca10acdb40ed5e966622b135
fushijia <2812719509@qq.com>
2019-03-01 16:30:44 +0800

    weixinjssdk

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -17,6 +17,7 @@ class WechatController extends Controller
 {
     //
     protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
+    protected $redis_weixin_jsapi_ticket = 'str:weixin_jsapi_ticket';     //微信 jsapi_ticket
 
     public function test()
     {
